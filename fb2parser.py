293e4301c47be0d6e71f97f4153888bb3464226c
Aleksandr Gonchar <Aleksandr_gonchar@epam.com>
2020-05-13 20:15:54 +0300

    last changes

--- fb2parser.py
+++ fb2parser.py
@@ -108,103 +108,106 @@ for filename in os.listdir(path):
                                      'count_upper_case integer);'
             create_table(conn,create_book_table)
             print('table for words in a file created')
+            
+            # reading xml file
+            tree = ET.parse(f)
+            root = tree.getroot()
+    
+            # this thing sticked to every tag in the file, looks like this is common for fb2 files
+            xmlns = '{http://www.gribuser.ru/xml/fictionbook/2.0}'
+    
+            # get book_name
+            for book_name in root.findall('./' + xmlns + 'description/' + xmlns + 'title-info/' + xmlns + 'book-title'):
+                book_name = book_name.text
+                print('book_name received')
+    
+            # get number of paragraphs
+            p_tags = 0
+            for p in root.iter(xmlns + 'p'):
+                p_tags = p_tags + 1
+            print('num of paragraphs received')
+    
+            # get all text from the book_name
+            all_text = ''
+            for element in root.iter():
+                all_text = all_text + str(element.text) + ' '
+            all_text = all_text[:-1]  # Remove trailing space
+            print('all text from file received')
+    
+            # filter text to get only cyrillic letters and spaces
+            filtered_text = ''
+            for char in all_text:
+                if (has_cyrillic(char)) or (char == ' '):
+                    filtered_text += char
+            print('text filtered')
+    
+            # get all words
+            words = filtered_text.split()
+            print('all words from text received')
+    
+            # count number of all words
+            n_words = len(words)
+            print('num of words received')
+    
+            # count all cyrillic letters (skipping spaces)
+            n_letters = 0
+            for letter in filtered_text:
+                if (has_cyrillic(letter)):
+                    n_letters += 1
+            print('num of letters received')
+    
+            # sort all words on lower and capitalized
+            lower_words = []
+            cap_words = []
+            sort_low_cap(words, lower_words, cap_words)
+            print('sort on lowercase words and words with caps')
+    
+            # counting lower and cap words
+            n_lower_words = len(lower_words)
+            print('num of lower_words received')
+            n_cap_words = len(cap_words)
+            print('num of cap_words received')
+    
+            # inserting info about the book to allbooks tab
+            book = (book_name, p_tags, n_words, n_letters, n_cap_words, n_lower_words)
+            insert_book(conn, book)
+            conn.commit()
+            print('book info inserted into allbooks table')
+    
+            # get all words and make them lowercase
+            lowered_words = []
+            words_to_lower(words, lowered_words)
+            print('all words converted to lower case received')
+    
+            # get all words with capitals and make them lowercase
+            lowered_cap_words = []
+            words_to_lower(cap_words, lowered_cap_words)
+            print('Cap words converted to lower case received')
+    
+            # Count frequency of all words and insert into books table
+            freq = {}
+            CountFrequency(lowered_words, freq)
+            for key, value in freq.items():
+                word_count = key, value
+                insert_words(conn, word_count)
+            conn.commit()
+            print('all words with count inserted into book table')
+    
+            # Count frequency of words with caps and update books table
+            freq = {}
+            CountFrequency(lowered_cap_words, freq)
+            for key, value in freq.items():
+                word_count = (value, key)
+                update_words(conn, word_count)
+            conn.commit()
+            print('Cap words count updated in book table')
+            
+            
         # moving other files to incorrect_input folder
         else:
             shutil.move(path+filename, path + 'incorrect_input/')
             print('incorrect files moved to incorrect_input')
 
-#reading xml file
-tree = ET.parse(f)
-root = tree.getroot()
-
-#this thing sticked to every tag in the file, looks like this is common for fb2 files
-xmlns='{http://www.gribuser.ru/xml/fictionbook/2.0}'
-
-#get book_name
-for book_name in root.findall('./'+xmlns+'description/'+xmlns+'title-info/'+xmlns+'book-title'):
-    book_name = book_name.text
-    print('book_name received')
-
-#get number of paragraphs
-p_tags = 0
-for p in root.iter(xmlns+'p'):
-    p_tags = p_tags + 1
-print('num of paragraphs received')
-
-#get all text from the book_name
-all_text = ''
-for element in root.iter():
-    all_text = all_text + str(element.text) + ' '
-all_text = all_text[:-1] # Remove trailing space
-print('all text from file received')
-
-#filter text to get only cyrillic letters and spaces
-filtered_text = ''
-for char in all_text:
-    if (has_cyrillic(char)) or (char == ' '):
-        filtered_text += char
-print('text filtered')
-
-#get all words
-words = filtered_text.split()
-print('all words from text received')
-
-#count number of all words
-n_words = len(words)
-print('num of words received')
-
-#count all cyrillic letters (skipping spaces)
-n_letters = 0
-for letter in filtered_text:
-    if (has_cyrillic(letter)):
-        n_letters += 1
-print('num of letters received')
-
-#sort all words on lower and capitalized
-lower_words = []
-cap_words = []
-sort_low_cap(words, lower_words, cap_words)
-print('sort on lowercase words and words with caps')
-
-#counting lower and cap words
-n_lower_words = len(lower_words)
-print('num of lower_words received')
-n_cap_words = len(cap_words)
-print('num of cap_words received')
-
-#inserting info about the book to allbooks tab
-book = (book_name, p_tags, n_words, n_letters, n_cap_words, n_lower_words)
-insert_book(conn,book)
-conn.commit()
-print('book info inserted into allbooks table')
-
-#get all words and make them lowercase
-lowered_words = []
-words_to_lower(words, lowered_words )
-print('all words converted to lower case received')
-
-#get all words with capitals and make them lowercase
-lowered_cap_words = []
-words_to_lower(cap_words, lowered_cap_words)
-print('Cap words converted to lower case received')
-
-#Count frequency of all words and insert into books table
-freq = {}
-CountFrequency(lowered_words, freq)
-for key, value in freq.items():
-    word_count = key, value
-    insert_words(conn, word_count)
-conn.commit()
-print('all words with count inserted into book table')
-
-#Count frequency of words with caps and update books table
-freq = {}
-CountFrequency(lowered_cap_words, freq)
-for key, value in freq.items():
-    word_count = (value, key)
-    update_words(conn, word_count)
-conn.commit()
-print('Cap words count updated in book table')
 
 # cur.execute('SELECT * FROM Allbooks')
 # results = cur.fetchall()
